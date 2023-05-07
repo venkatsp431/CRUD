c840562c931872c1300974e7652daa61dda6abd5
venkatsp431 <sealvenki@gmail.com>
2023-05-07 13:20:03 +0530

    Studentsdata

---
+++ src/Components/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Base from "../Base/base";
+import { useHistory } from "react-router-dom";
+
+export function Dashboard() {
+  const history = useHistory();
+  return (
+    <Base
+      title={"Welcome to Student Dasboard"}
+      description={"Here, you can view students data"}
+    >
+      <div>
+        <button onClick={() => history.push("/students")}>View Students</button>
+      </div>
+    </Base>
+  );
+}
